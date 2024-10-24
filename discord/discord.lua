bc865b66b4b4f9143366f495c3fd031053868000
크시 <168262498+xsinew@users.noreply.github.com>
2024-10-24 21:25:55 +0900

    Update discord.lua

--- discord/discord.lua
+++ discord/discord.lua
@@ -1,13 +1,15 @@
-local old_require = require
-getgenv()['require'] = function(asset)
-    if typeof(asset) == "string" then
-        if not isfile(asset) then
-            return loadstring(game:HttpGet("https://raw.githubusercontent.com/xsinew/discord.lua/refs/heads/main/"..asset))()
+if not getgenv()["old_require"] then
+    getgenv()["old_require"] = require
+    getgenv()['require'] = function(asset)
+        if typeof(asset) == "string" then
+            if not isfile(asset) then
+                return loadstring(game:HttpGet("https://raw.githubusercontent.com/xsinew/discord.lua/refs/heads/main/"..asset))()
+            else
+                return loadfile(asset)()
+            end
         else
-            return loadfile(asset)()
+            return getgenv()["old_require"](asset)
         end
-    else
-        return old_require(asset)
     end
 end
 
